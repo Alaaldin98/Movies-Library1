6beefb3090d85e79797cdad93517eefa1cea1ed6
Alaaldin98 <alaaalhanene@gmail.com>
2022-02-25 16:18:12 +0300

    Creat API

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const express = require("express");
 const recipes = require("./data.json");
 const app = express();
+const dotenv = require("dotenv");
+const axios = require("axios");
+dotenv.config();
+const APIKEY = process.env.APIKEY;
+const PORT = process.env.PORT;
+
 
 function Recipe(title,poster_path,overview ){
     this.title = title;
@@ -10,8 +16,11 @@ function Recipe(title,poster_path,overview ){
 };
 app.get(`/`,dataHandler);
 app.get(`/favorite`,favoriteHandler);
+app.get(`/trending`,trendingHandler);
 app.use("*", notFoundHandler);
 app.use("*", errorHandler);
+app.get("/search", searchRecipesHandler)
+
 
 function dataHandler(req, res){
     let result = [];
@@ -21,6 +30,38 @@ function dataHandler(req, res){
     });
     return res.status(200).json(result);
 };
+
+function trendingHandler(req, res){
+    let result = [];
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`)
+    .then(apiResponse => {
+        apiResponse.data.recipes.map(value => {
+            let oneRecipe = new Recipe(value.id,value.title,value.release_date, value.poster_path,value.overview);
+            result.push(oneRecipe);
+        })
+        return res.status(200).json(result);
+    }).catch(error => {
+        errorHandler(error, req, res);
+    })    
+};
+
+function searchRecipesHandler(req, res){
+    const search = req.query.recipe
+    let results = [];
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${search}`)
+    .then(apiResponse=>{
+        apiResponse.data.results.map(value => {
+            let oneRecipe = new Recipe(value.id || "N/A", value.title || "N/A", value.readyInMinutes || "N/A", value.vegetarian || "N/A", value.sourceUrl || "N/A", value.image || "N/A", value.summary || "N/A", value.instructions || "N/A")
+            results.push(oneRecipe);
+        });
+        return res.status(200).json(results);
+    }).catch(error => {
+        errorHandler(error, req, res);
+    })
+
+}
+
+
 
 function favoriteHandler(request, response){
     
@@ -39,6 +80,6 @@ function errorHandler(error,req,res){
     return res.status(500).send(err);
 }
 
-app.listen(3000, () => {
-    console.log("Listen on 3000");
+app.listen(PORT, () => {
+    console.log(`Listen on ${PORT}`);
 });
